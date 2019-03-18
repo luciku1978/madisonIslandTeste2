588aaef0029712551525cf803529ae35ee219bed
agtourcj3 <lucian.maniu@gmail.com>
2019-03-18 23:06:28 +0200

    Assertions pentru suitele de teste RegisterTest si LoginTest

--- src/test/java/org/fasttrackit/LoginTest.java
+++ src/test/java/org/fasttrackit/LoginTest.java
@@ -6,10 +6,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.*;
+
 public class LoginTest {
 
     @Test
     public void loginAnAlreadyRegisteredUser() throws InterruptedException {
+
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://fasttrackit.org/selenium-test/");
@@ -20,9 +25,10 @@ public class LoginTest {
 
         WebElement loginLink = driver.findElement(By.xpath("//div[@class='links']//a[@title='Log In']"));
         loginLink.click();
+        String valid_email = "lucian_lrl@yahoo.com";
 
         WebElement email = driver.findElement(By.id("email"));
-        email.sendKeys("lucian_lrl@yahoo.com");
+        email.sendKeys(valid_email);
 
         Thread.sleep(2500);
 
@@ -37,6 +43,12 @@ public class LoginTest {
 
         Thread.sleep(2500);
 
+        WebElement contactInfo = driver.findElement(By.cssSelector(".box-content p"));
+        String contactEmail = contactInfo.getText();
+
+        assertThat("The contact info page does not display the correct e-mail adress",contactEmail,containsString(valid_email));
+
+        Thread.sleep(2500);
         driver.quit();
     }
     @Test
@@ -65,6 +77,14 @@ public class LoginTest {
 
         WebElement loginButton = driver.findElement(By.xpath("//span[text() ='Login']"));
         loginButton.click();
+
+        Thread.sleep(2500);
+
+        WebElement errorMessageContainer = driver.findElement(By.cssSelector(".error-msg span"));
+        String error_message = errorMessageContainer.getText();
+
+        assertThat("The error message 'Invalid  login or password.' does not appear on the page!",error_message,
+                containsString("Invalid login or password."));
 
         Thread.sleep(2500);
 
@@ -101,6 +121,14 @@ public class LoginTest {
 
         Thread.sleep(2500);
 
+        WebElement errorMessageContainer = driver.findElement(By.cssSelector(".error-msg span"));
+        String error_message = errorMessageContainer.getText();
+
+        assertThat("The error message 'Invalid  login or password.' does not appear on the page!",error_message,
+                containsString("Invalid login or password."));
+
+        Thread.sleep(2500);
+
         driver.quit();
 
     }
@@ -121,18 +149,18 @@ public class LoginTest {
         WebElement email = driver.findElement(By.id("email"));
         email.sendKeys("lucian_lrl@yahoo.com");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
 
         WebElement password = driver.findElement(By.id("pass"));
         password.sendKeys("eduard2008");
 
-        Thread.sleep(2500);
+        Thread.sleep(2000);
 
         WebElement loginButton = driver.findElement(By.xpath("//span[text() ='Login']"));
         loginButton.click();
 
-        Thread.sleep(1500);
+        Thread.sleep(2500);
         WebElement accountBtn1 = driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[@class='label']"));
         accountBtn1.click();;
 
@@ -140,8 +168,20 @@ public class LoginTest {
         WebElement logoutLink = driver.findElement(By.xpath("//div[@class='links']//a[@title='Log Out']"));
         logoutLink.click();
 
-        Thread.sleep(10000);
+        WebElement logoutMessageContainer = driver.findElement(By.cssSelector(".page-title h1"));
+        String logout_confirmation_message = logoutMessageContainer.getText().toLowerCase();
 
+        assertThat("The logout confirmation message is not properly displayed!",
+                logout_confirmation_message,containsString("logged out"));
+
+        Thread.sleep(5000);
+
+        String homepage =driver.getCurrentUrl();
+
+        assertThat("The logged-out user was not redirected to homepage!",
+                homepage,equalTo("https://fasttrackit.org/selenium-test/"));
+
+        Thread.sleep(10000);
         driver.quit();
     }
 }
--- src/test/java/org/fasttrackit/RegisterTest.java
+++ src/test/java/org/fasttrackit/RegisterTest.java
@@ -6,7 +6,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.security.SecureRandom;
 import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RegisterTest {
 
@@ -20,42 +24,47 @@ public class RegisterTest {
         WebElement accountBtn = driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[@class='label']"));
         accountBtn.click();
 
+        Thread.sleep(1000);
+
         WebElement registerBtn = driver.findElement(By.xpath("//div[@class='links']//a[@title='Register']"));
         registerBtn.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
+        String first_name = "Garry";
         WebElement firstName = driver.findElement(By.id("firstname"));
-        firstName.sendKeys("Albert");
+        firstName.sendKeys(first_name);
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
+        String middle_name = "Alec";
         WebElement middleName = driver.findElement(By.id("middlename"));
-        middleName.sendKeys("Gaudi");
+        middleName.sendKeys(middle_name);
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
+        String last_name = "Wolf";
         WebElement lastName = driver.findElement(By.name("lastname"));
-        lastName.sendKeys("Velmar");
+        lastName.sendKeys(last_name);
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         final String uniqueEmail = generateUniqueEmail();
 
         WebElement email = driver.findElement(By.className("validate-email"));
         email.sendKeys(uniqueEmail);
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement password = driver.findElement(By.id("password"));
         password.sendKeys("123456");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement confirmPasword = driver.findElement(By.name("confirmation"));
         confirmPasword.sendKeys("123456");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
         signUpForNewsletter.click();
@@ -65,13 +74,35 @@ public class RegisterTest {
         WebElement register = driver.findElement(By.xpath("//div[@class='buttons-set']//button[@title='Register']"));
         register.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1500);
+
+        WebElement successMsgContainer = driver.findElement(By.cssSelector(".success-msg span"));
+        String successMessage = successMsgContainer.getText();
+
+        assertThat("Missing succes message on the registration page!",
+                successMessage, containsString("Thank you for registering with Madison Island."));
+
+        WebElement wellcomeMsgContainer = driver.findElement(By.cssSelector(".welcome-msg "));
+        String wellcomeMsg = wellcomeMsgContainer.getText();
+
+        assertThat("Missing wellcome message on the top of the page containing the name of the user. ",
+                wellcomeMsg,
+                containsString(first_name.toUpperCase() + " " + middle_name.toUpperCase() + " " + last_name.toUpperCase()));
 
         driver.quit();
     }
 
     private static String generateUniqueEmail() {
-        return "random-" + UUID.randomUUID().toString() + "@email.com";
+        final String AB = "0123456789";
+        SecureRandom rnd = new SecureRandom();
+
+        int len = 5;
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+
+
+        return "random-email" + sb.toString() + "@email.com";
     }
 
     @Test
@@ -83,51 +114,60 @@ public class RegisterTest {
         WebElement accountBtn = driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[@class='label']"));
         accountBtn.click();
 
+        Thread.sleep(1000);
+
         WebElement registerBtn = driver.findElement(By.xpath("//div[@class='links']//a[@title='Register']"));
         registerBtn.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement firstName = driver.findElement(By.id("firstname"));
         firstName.sendKeys("paul");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement middleName = driver.findElement(By.id("middlename"));
         middleName.sendKeys("goma");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement lastName = driver.findElement(By.name("lastname"));
         lastName.sendKeys("marin");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement email = driver.findElement(By.className("validate-email"));
         email.sendKeys("lucian_lrl@yahoo.com");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement password = driver.findElement(By.id("password"));
         password.sendKeys("123456");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement confirmPasword = driver.findElement(By.name("confirmation"));
         confirmPasword.sendKeys("123456");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
         signUpForNewsletter.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement register = driver.findElement(By.xpath("//div[@class='buttons-set']//button[@title='Register']"));
         register.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
+        WebElement errorMsgContainer = driver.findElement(By.cssSelector(".error-msg span"));
+        String errorMessage = errorMsgContainer.getText();
+
+        assertThat("There is no error message on page stating another account has the same email adress!",
+                errorMessage, containsString("There is already an account with this email address."));
+
+        Thread.sleep(3000);
         driver.quit();
 
     }
@@ -141,46 +181,55 @@ public class RegisterTest {
         WebElement accountBtn = driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[@class='label']"));
         accountBtn.click();
 
+        Thread.sleep(1000);
+
         WebElement registerBtn = driver.findElement(By.xpath("//div[@class='links']//a[@title='Register']"));
         registerBtn.click();
 
-        Thread.sleep(2500);
-//        WebElement firstName = driver.findElement(By.id("firstname"));
-//        firstName.sendKeys("paul");
+        Thread.sleep(1000);
+        WebElement firstName = driver.findElement(By.id("firstname"));
+        firstName.sendKeys("");
 
         WebElement middleName = driver.findElement(By.id("middlename"));
         middleName.sendKeys("mike");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement lastName = driver.findElement(By.name("lastname"));
         lastName.sendKeys("martin");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement email = driver.findElement(By.className("validate-email"));
         email.sendKeys("martin@yahoo.com");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement password = driver.findElement(By.id("password"));
         password.sendKeys("123456");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement confirmPasword = driver.findElement(By.name("confirmation"));
         confirmPasword.sendKeys("123456");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
+        WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
+        signUpForNewsletter.click();
 
-//        WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
-//        signUpForNewsletter.click();
+        Thread.sleep(1000);
 
         WebElement register = driver.findElement(By.xpath("//div[@class='buttons-set']//button[@title='Register']"));
         register.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
+
+        WebElement adviceContainer = driver.findElement(By.cssSelector(".validation-advice"));
+        String validation_advice = adviceContainer.getText();
+
+        assertThat("The validation advice for missing firstname is not present on page!",
+                validation_advice,containsString("This is a required field."));
 
         driver.quit();
 
@@ -195,48 +244,58 @@ public class RegisterTest {
         WebElement accountBtn = driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[@class='label']"));
         accountBtn.click();
 
+        Thread.sleep(1000);
+
         WebElement registerBtn = driver.findElement(By.xpath("//div[@class='links']//a[@title='Register']"));
         registerBtn.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement firstName = driver.findElement(By.id("firstname"));
         firstName.sendKeys("george");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement middleName = driver.findElement(By.id("middlename"));
         middleName.sendKeys("m");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement lastName = driver.findElement(By.name("lastname"));
         lastName.sendKeys("busch");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement email = driver.findElement(By.className("validate-email"));
         email.sendKeys("");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement password = driver.findElement(By.id("password"));
         password.sendKeys("123456");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement confirmPasword = driver.findElement(By.name("confirmation"));
         confirmPasword.sendKeys("123456");
 
+        Thread.sleep(1000);
 
-        Thread.sleep(2500);
-//        WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
-//        signUpForNewsletter.click();
+        WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
+        signUpForNewsletter.click();
+
+        Thread.sleep(1000);
 
         WebElement register = driver.findElement(By.xpath("//div[@class='buttons-set']//button[@title='Register']"));
         register.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
+
+        WebElement adviceContainer = driver.findElement(By.cssSelector(".validation-advice"));
+        String validation_advice = adviceContainer.getText();
+
+        assertThat("The validation advice for missing email is not present on page!",
+                validation_advice,containsString("This is a required field."));
 
         driver.quit();
     }
@@ -251,54 +310,66 @@ public class RegisterTest {
         WebElement accountBtn = driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[@class='label']"));
         accountBtn.click();
 
+        Thread.sleep(1000);
+
         WebElement registerBtn = driver.findElement(By.xpath("//div[@class='links']//a[@title='Register']"));
         registerBtn.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement firstName = driver.findElement(By.id("firstname"));
         firstName.sendKeys("oancea");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement middleName = driver.findElement(By.id("middlename"));
-        middleName.sendKeys("val");
+        middleName.sendKeys("vali");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement lastName = driver.findElement(By.name("lastname"));
         lastName.sendKeys("mihai");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement email = driver.findElement(By.className("validate-email"));
         email.sendKeys("oancea@yahoo.com");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement password = driver.findElement(By.id("password"));
         password.sendKeys("123");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement confirmPasword = driver.findElement(By.name("confirmation"));
         confirmPasword.sendKeys("123");
 
+        Thread.sleep(1000);
 
-        Thread.sleep(2500);
-//        WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
-//        signUpForNewsletter.click();
+        WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
+        signUpForNewsletter.click();
+
+        Thread.sleep(1000);
 
         WebElement register = driver.findElement(By.xpath("//div[@class='buttons-set']//button[@title='Register']"));
         register.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
+        WebElement adviceContainer = driver.findElement(By.cssSelector(".validation-advice"));
+        String validation_advice = adviceContainer.getText();
+
+        assertThat("The validation advice for incorrect password is not present on page!",
+                validation_advice,
+                containsString("Please enter 6 or more characters without leading or trailing spaces."));
+
+        Thread.sleep(3000);
         driver.quit();
     }
 
     @Test
-    public void addUserWhenPasswordsDontMatch () throws InterruptedException {
+    public void addUserWhenPasswordsDontMatch() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://fasttrackit.org/selenium-test/");
         System.out.println("Opened homepage.");
@@ -306,54 +377,66 @@ public class RegisterTest {
         WebElement accountBtn = driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[@class='label']"));
         accountBtn.click();
 
+        Thread.sleep(1000);
+
         WebElement registerBtn = driver.findElement(By.xpath("//div[@class='links']//a[@title='Register']"));
         registerBtn.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement firstName = driver.findElement(By.id("firstname"));
-        firstName.sendKeys("alecu");
+        firstName.sendKeys("alec");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement middleName = driver.findElement(By.id("middlename"));
         middleName.sendKeys("ryan");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement lastName = driver.findElement(By.name("lastname"));
         lastName.sendKeys("bill");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement email = driver.findElement(By.className("validate-email"));
         email.sendKeys("bill@yahoo.com");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement password = driver.findElement(By.id("password"));
         password.sendKeys("11111111");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement confirmPasword = driver.findElement(By.name("confirmation"));
         confirmPasword.sendKeys("11234564");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
-//        WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
-//        signUpForNewsletter.click();
+        WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
+        signUpForNewsletter.click();
+
+        Thread.sleep(1000);
 
         WebElement register = driver.findElement(By.xpath("//div[@class='buttons-set']//button[@title='Register']"));
         register.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
+
+        WebElement adviceContainer = driver.findElement(By.cssSelector(".validation-advice"));
+        String validation_advice = adviceContainer.getText();
+
+        assertThat("The validation advice for non matching passwords is not present on page!",
+                validation_advice,
+                containsString("Please make sure your passwords match."));
+
 
         driver.quit();
     }
 
     @Test
-    public void addUserWithInvalidEmailAdress () throws InterruptedException {
+    public void addUserWithInvalidEmailAdress() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://fasttrackit.org/selenium-test/");
         System.out.println("Opened homepage.");
@@ -361,43 +444,47 @@ public class RegisterTest {
         WebElement accountBtn = driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//span[@class='label']"));
         accountBtn.click();
 
+        Thread.sleep(1000);
+
         WebElement registerBtn = driver.findElement(By.xpath("//div[@class='links']//a[@title='Register']"));
         registerBtn.click();
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement firstName = driver.findElement(By.id("firstname"));
         firstName.sendKeys("calin");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement middleName = driver.findElement(By.id("middlename"));
-        middleName.sendKeys("file");
+        middleName.sendKeys("flo");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement lastName = driver.findElement(By.name("lastname"));
-        lastName.sendKeys("poveste");
+        lastName.sendKeys("popovici");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement email = driver.findElement(By.className("validate-email"));
         email.sendKeys("calinyahoo.com");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement password = driver.findElement(By.id("password"));
         password.sendKeys("000000");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
         WebElement confirmPasword = driver.findElement(By.name("confirmation"));
         confirmPasword.sendKeys("000000");
 
-        Thread.sleep(2500);
+        Thread.sleep(1000);
 
-//        WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
-//        signUpForNewsletter.click();
+        WebElement signUpForNewsletter = driver.findElement(By.className("checkbox"));
+        signUpForNewsletter.click();
+
+        Thread.sleep(1000);
 
         WebElement register = driver.findElement(By.xpath("//div[@class='buttons-set']//button[@title='Register']"));
         register.click();
